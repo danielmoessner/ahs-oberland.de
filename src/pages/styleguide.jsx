7acaed53700fe94616d3f48f5a0fbdc4895b1e83
danielmoessner <github@moesibert.de>
2021-05-06 12:12:55 +0200

    updates

--- src/pages/styleguide.jsx
+++ src/pages/styleguide.jsx
@@ -9,6 +9,7 @@ import Pre from '../components/Pre';
 import Heading from '../components/Heading';
 import Button from '../components/Button';
 import DynamicForm from '../components/DynamicForm';
+import Container from '../components/Container';
 
 function Page({ data }) {
   const page = data.pagesYaml;
@@ -65,87 +66,93 @@ function Page({ data }) {
         description={page.meta.description}
         image={page.meta.image.childImageSharp.resize.src}
       />
-      <div className="mt-12 mb-24">
-        <Heading element="h1">Styleguide</Heading>
-      </div>
-      <div className="space-y-16">
-        <div className="">
-          <Pre element="h2">Typographie</Pre>
-          <div className="mt-8 space-y-8">
-            <div className="flex items-baseline justify-between">
-              <Heading element="div" size="h1">
-                {headingText}
-              </Heading>
-              <div>H1</div>
+      <section>
+        <Container>
+          <div className="bg-white py-32 px-8">
+            <div className="mb-24">
+              <Heading element="h1">Styleguide</Heading>
             </div>
-            <div className="flex items-baseline justify-between">
-              <Heading element="div" size="h2">
-                {headingText}
-              </Heading>
-              <div>H2</div>
-            </div>
-            <div className="flex items-baseline justify-between">
-              <Heading element="div" size="h3">
-                {headingText}
-              </Heading>
-              <div>H3</div>
-            </div>
-            <div className="flex items-baseline justify-between">
-              <Pre>{headingText}</Pre>
-              <div>Pre</div>
+            <div className="space-y-16">
+              <div className="">
+                <Pre element="h2">Typographie</Pre>
+                <div className="mt-8 space-y-8">
+                  <div className="flex items-baseline justify-between">
+                    <Heading element="div" size="h1">
+                      {headingText}
+                    </Heading>
+                    <div>H1</div>
+                  </div>
+                  <div className="flex items-baseline justify-between">
+                    <Heading element="div" size="h2">
+                      {headingText}
+                    </Heading>
+                    <div>H2</div>
+                  </div>
+                  <div className="flex items-baseline justify-between">
+                    <Heading element="div" size="h3">
+                      {headingText}
+                    </Heading>
+                    <div>H3</div>
+                  </div>
+                  <div className="flex items-baseline justify-between">
+                    <Pre>{headingText}</Pre>
+                    <div>Pre</div>
+                  </div>
+                </div>
+              </div>
+              <div className="">
+                <Pre element="h2">Buttons</Pre>
+                <div className="mt-8 space-y-8">
+                  <div className="flex items-baseline justify-between">
+                    <Button to="/styleguide/">Button</Button>
+                    <div>(Primary, Link-Tag)</div>
+                  </div>
+                  <div className="flex items-baseline justify-between">
+                    <Button to="/styleguide/" importance="secondary">
+                      Button
+                    </Button>
+                    <div>(Secondary, Link-Tag)</div>
+                  </div>
+                  <div className="flex items-baseline justify-between">
+                    <Button href="/styleguide/" element="a">
+                      Button
+                    </Button>
+                    <div>(Primary, A-Tag)</div>
+                  </div>
+                  <div className="flex items-baseline justify-between">
+                    <Button
+                      type="submit"
+                      element="button"
+                      colors="bg-gray-200 hover:bg-gray-800 ring-offset-white transition focus:ring-gray-900 text-black hover:text-white"
+                    >
+                      Button
+                    </Button>
+                    <div>(Primary, Button-Tag, Colors)</div>
+                  </div>
+                  <div className="flex items-baseline justify-between">
+                    <Button to="/styleguide/" ringOffsetClass="ring-offset-black">
+                      Button
+                    </Button>
+                    <div>(Primary, Link-Tag, RingOffsetBlack)</div>
+                  </div>
+                  <div className="flex items-baseline justify-between">
+                    <Button to="/styleguide/" size="px-6 py-3 text-base">
+                      Button Big
+                    </Button>
+                    <div>(Primary, Size)</div>
+                  </div>
+                </div>
+              </div>
+              <div className="">
+                <Pre element="h2">Formulare</Pre>
+                <div className="mt-8 space-y-8">
+                  <DynamicForm fields={fields} />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="">
-          <Pre element="h2">Buttons</Pre>
-          <div className="mt-8 space-y-8">
-            <div className="flex items-baseline justify-between">
-              <Button to="/styleguide/">Button</Button>
-              <div>(Primary, Link-Tag)</div>
-            </div>
-            <div className="flex items-baseline justify-between">
-              <Button to="/styleguide/" importance="secondary">
-                Button
-              </Button>
-              <div>(Secondary, Link-Tag)</div>
-            </div>
-            <div className="flex items-baseline justify-between">
-              <Button href="/styleguide/" element="a">
-                Button
-              </Button>
-              <div>(Primary, A-Tag)</div>
-            </div>
-            <div className="flex items-baseline justify-between">
-              <Button
-                type="submit"
-                element="button"
-                colors="bg-gray-200 hover:bg-gray-800 ring-offset-white transition focus:ring-gray-900 text-black hover:text-white"
-              >
-                Button
-              </Button>
-              <div>(Primary, Button-Tag, Colors)</div>
-            </div>
-            <div className="flex items-baseline justify-between">
-              <Button to="/styleguide/" ringOffsetClass="ring-offset-black">
-                Button
-              </Button>
-              <div>(Primary, Link-Tag, RingOffsetBlack)</div>
-            </div>
-            <div className="flex items-baseline justify-between">
-              <Button to="/styleguide/" size="px-6 py-3 text-base">
-                Button Big
-              </Button>
-              <div>(Primary, Size)</div>
-            </div>
-          </div>
-        </div>
-        <div className="">
-          <Pre element="h2">Formulare</Pre>
-          <div className="mt-8 space-y-8">
-            <DynamicForm fields={fields} />
-          </div>
-        </div>
-      </div>
+        </Container>
+      </section>
     </Layout>
   );
 }
